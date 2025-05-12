4f0bbc02251c2705c6d8b9599f1bc6969eb08e18
Anthony Trépanier <anthony.trepanier@hotmail.com>
2025-05-12 14:58:54 -0400

    add destination component

---
+++ client/app/components/Destination/Destination.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Destination = (props: {
+  name: string;
+  description: string;
+  distance: string;
+  travelTime: string;
+}) => {
+  return (
+    <div className="flex flex-col space-y-6 justify-center font-bellefair">
+      {/** Content */}
+      <div className=" flex flex-col space-y-6 text-center">
+        <h2 className="text-white uppercase">{props.name}</h2>
+        <p className="text-light-blue">{props.description}</p>
+      </div>
+      {/** Border */}
+      <div className="w-full h-[0.1px] bg-grey"></div>
+      {/** Stats */}
+      <div className="flex flex-col space-y-6">
+        {/** Stat Distance */}
+        <div className="destination-stat-container">
+          <h5 className="text-light-blue">avg. distance</h5>
+          <h4 className="text-white">{props.distance}</h4>
+        </div>
+        {/** Stat Travel Time */}
+        <div className="destination-stat-container">
+          <h5 className="text-light-blue">est. travel time</h5>
+          <h4 className="text-white">{props.travelTime}</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Destination;
